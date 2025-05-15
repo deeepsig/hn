d21f4af5fade4afce1e723fe0252da02dd50b9c5
deeepsig <deepakgdkg1g8868@gmail.com>
2025-05-15 15:01:07 +0530

    minor style changes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,9 +8,9 @@ export default function Navbar() {
     <header className="flex items-center justify-between bg-white border-b border-gray-200 h-16 px-8 font-inter">
       {/* Left section - Logo and Name */}
       <div className="flex items-center">
-        <img src={hnLogo} alt="Hacker News" className="h-5 mr-3" />
+        <img src={hnLogo} alt="SPC News" className="h-5 mr-3" />
         <span className="font-semibold text-gray-700 text-base">
-          Hacker News
+          SPC Maker News
         </span>
       </div>
 
