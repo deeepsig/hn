2f27322d0f90ee965ee392a47e9eb8633d82bdc5
deeepsig <deepakgdkg1g8868@gmail.com>
2025-05-15 12:58:14 +0530

    stylistic improvements

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,63 +1,77 @@
 import {
   MagnifyingGlass,
   User,
-  BookmarkSimple,
   Clock,
-  CaretDown
+  CaretDown,
+  TextAa
 } from '@phosphor-icons/react'
 import hnLogo from '../../assets/hn.png'
 
 export default function Navbar() {
   return (
-    <header className="flex items-center justify-between bg-white border-b border-gray-200 px-2 h-12 font-inter">
+    <header className="flex items-center justify-between bg-white border-b border-gray-200 px-6 py-3 font-inter">
       {/* Left section - Logo and Name */}
       <div className="flex items-center">
-        <img src={hnLogo} alt="Hacker News" className="h-6 mr-2" />
-        <span className="font-semibold text-gray-700">Hacker News</span>
+        <img src={hnLogo} alt="Hacker News" className="h-5 mr-3" />
+        <span className="font-semibold text-gray-700 text-base">
+          Hacker News
+        </span>
       </div>
       {/* Middle section - Navigation links */}
-      <nav className="hidden md:flex items-center space-x-4">
+      <nav className="hidden md:flex items-center space-x-6">
         <a
           href="#"
-          className="px-2 py-1 text-black border-b-2 border-orange-500"
+          className="px-3 py-2 text-black border-b-2 border-orange-500 text-base font-medium"
         >
           Top
         </a>
-        <a href="#" className="px-2 py-1 text-gray-500 hover:text-gray-800">
+        <a
+          href="#"
+          className="px-3 py-2 text-gray-500 hover:text-gray-800 text-base"
+        >
           Ask
         </a>
-        <a href="#" className="px-2 py-1 text-gray-500 hover:text-gray-800">
+        <a
+          href="#"
+          className="px-3 py-2 text-gray-500 hover:text-gray-800 text-base"
+        >
           Show
         </a>
-        <a href="#" className="px-2 py-1 text-gray-500 hover:text-gray-800">
+        <a
+          href="#"
+          className="px-3 py-2 text-gray-500 hover:text-gray-800 text-base"
+        >
           Best
         </a>
-        <a href="#" className="px-2 py-1 text-gray-500 hover:text-gray-800">
+        <a
+          href="#"
+          className="px-3 py-2 text-gray-500 hover:text-gray-800 text-base"
+        >
           New
         </a>
-        <a href="#" className="px-2 py-1 text-gray-500 hover:text-gray-800">
+        <a
+          href="#"
+          className="px-3 py-2 text-gray-500 hover:text-gray-800 text-base"
+        >
           Active
         </a>
         <button className="text-gray-500 flex items-center">
-          <CaretDown size={16} />
+          <CaretDown size={20} />
         </button>
       </nav>
       {/* Right section - Icons */}
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center space-x-6">
         <button className="text-gray-500">
-          <span className="text-lg">Aa</span>
+          <TextAa size={24} weight="regular" />
         </button>
         <button className="text-gray-500">
-          <Clock size={20} />
+          <Clock size={24} />
         </button>
         <button className="text-gray-500">
-          <BookmarkSimple size={20} />
+          <User size={24} />
         </button>
         <button className="text-gray-500">
-          <User size={20} />
-        </button>
-        <button className="text-gray-500">
-          <MagnifyingGlass size={20} />
+          <MagnifyingGlass size={24} />
         </button>
       </div>
     </header>
