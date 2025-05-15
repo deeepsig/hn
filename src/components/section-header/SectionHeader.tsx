73ea3f7b56a0e78f5ccbb6532d095d11c1630f0f
deeepsig <deepakgdkg1g8868@gmail.com>
2025-05-15 18:35:41 +0530

    stylystic improvements

--- src/components/section-header/SectionHeader.tsx
+++ src/components/section-header/SectionHeader.tsx
@@ -5,26 +5,24 @@ export default function SectionHeader() {
   const [activeTab, setActiveTab] = useState('Stories')
 
   return (
-    <div className="max-w-[600px] mx-auto pt-8 pb-6 border-b border-gray-200 font-inter">
+    <div className="max-w-[600px] mx-auto pt-8 pb-5 border-b border-gray-100 font-inter">
       <div className="flex items-center justify-between">
         <div className="flex items-center">
           <h1 className="text-[36px] font-medium text-gray-900 mr-10">New</h1>
 
-          <div className="flex rounded-md bg-gray-100 p-0.5">
+          <div className="flex rounded-full bg-gray-100 p-0.5">
             <button
-              className={`px-3 py-1 text-sm ${
-                activeTab === 'Stories'
-                  ? 'bg-white shadow-sm rounded-md'
-                  : 'text-gray-500'
+              className={`px-5 py-1.5 text-sm rounded-full ${
+                activeTab === 'Stories' ? 'bg-white shadow-sm' : 'text-gray-500'
               }`}
               onClick={() => setActiveTab('Stories')}
             >
               Stories
             </button>
             <button
-              className={`px-3 py-1 text-sm ${
+              className={`px-5 py-1.5 text-sm rounded-full ${
                 activeTab === 'Comments'
-                  ? 'bg-white shadow-sm rounded-md'
+                  ? 'bg-white shadow-sm'
                   : 'text-gray-500'
               }`}
               onClick={() => setActiveTab('Comments')}
@@ -34,8 +32,8 @@ export default function SectionHeader() {
           </div>
         </div>
 
-        <button>
-          <DotsThree size={32} className="text-gray-700" weight="bold" />
+        <button className="flex items-center h-10">
+          <DotsThree size={32} className="text-gray-600" weight="bold" />
         </button>
       </div>
     </div>
