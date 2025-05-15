ebd9ecccee87c25c7bb5867510ee0184e593905f
deeepsig <deepakgdkg1g8868@gmail.com>
2025-05-15 20:30:56 +0530

    Added a controller to handle section header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import Navbar from './components/navbar/Navbar'
-import OtherSectionHeader from './components/section-header/OtherSectionHeader'
-import TopSectionHeader from './components/section-header/TopSectionHeader'
+import SectionHeaderController from './components/section-header/SectionHeaderController'
 
 export default function App() {
   return (
     <div>
       <div className="">
         <Navbar />
-        {/* <OtherSectionHeader pageType="New" /> */}
-        <TopSectionHeader />
+        <SectionHeaderController pageType="Top" />
       </div>
     </div>
   )
