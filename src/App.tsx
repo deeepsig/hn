244585426063f02dca64b2f7569fe99a6ffc2553
deeepsig <deepakgdkg1g8868@gmail.com>
2025-05-16 17:48:14 +0530

    ThreadHeader UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import Navbar from './components/navbar/Navbar'
 import SectionHeaderController from './components/section-header/SectionHeaderController'
 import StoryList from './components/story/StoryList'
 import CommentList from './components/comments/CommentList'
+import ThreadHeaderController from './components/thread/ThreadHeaderController'
 
 export default function App() {
   return (
     <div>
       <div className="">
         <Navbar />
-        <SectionHeaderController pageType="New" />
+        {/* <SectionHeaderController pageType="New" /> */}
+        <ThreadHeaderController />
         <CommentList />
       </div>
     </div>
