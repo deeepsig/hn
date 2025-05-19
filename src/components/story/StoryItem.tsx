ff79588a7fd6869a04409c7efea5904703914a6d
deeepsig <deepakgdkg1g8868@gmail.com>
2025-05-19 14:27:45 +0530

    removed source from the Ask stories design

--- src/components/story/StoryItem.tsx
+++ src/components/story/StoryItem.tsx
@@ -1,5 +1,6 @@
 // src/components/story/StoryItem.tsx
 import { Link } from 'react-router-dom'
+import { useSectionPageContext } from '../../contexts/SectionPageContext'
 
 export interface StoryItemProps {
   index: number
@@ -24,6 +25,7 @@ export default function StoryItem({
   time,
   comments
 }: StoryItemProps) {
+  const { pageType } = useSectionPageContext()
   const displayIndex = rank ?? index
   const storyPath = `/story/${index}`
 
@@ -32,6 +34,7 @@ export default function StoryItem({
       <span className="mr-4 font-normal text-right text-gray-400">
         {displayIndex}.
       </span>
+
       <div className="flex-1">
         <a
           href={url}
@@ -42,41 +45,53 @@ export default function StoryItem({
         >
           {title}
         </a>
-        <div>
-          <a
-            href={source}
-            className="font-normal text-orange-500 hover:underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ({source})
-          </a>
-          <ul className="inline-flex flex-wrap items-center ml-2 space-x-2">
-            <li className="text-sm text-gray-400">·</li>
-            <li>
-              <Link to={storyPath} className="hover:underline">
-                {points} pts
-              </Link>
-            </li>
-            <li className="text-sm text-gray-400">·</li>
-            <li>
-              <Link to={storyPath} className="hover:underline">
-                by {author}
-              </Link>
-            </li>
-            <li className="text-sm text-gray-400">·</li>
-            <li>
-              <Link to={storyPath} className="hover:underline">
-                {time}
-              </Link>
-            </li>
-            <li className="text-sm text-gray-400">·</li>
-            <li>
-              <Link to={storyPath} className="hover:underline">
-                {comments} comments
-              </Link>
-            </li>
-          </ul>
+
+        <div className="inline-flex flex-wrap items-center space-x-2">
+          {/* Source (hidden on Ask pages) */}
+          {pageType !== 'Ask' && (
+            <span className="inline-flex items-center space-x-2">
+              <a
+                href={source}
+                className="font-normal text-orange-500 hover:underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ({source})
+              </a>
+              <span className="text-sm text-gray-400">·</span>
+            </span>
+          )}
+
+          {/* Points */}
+          <span className="inline-flex items-center space-x-2">
+            <Link to={storyPath} className="hover:underline">
+              {points} pts
+            </Link>
+            <span className="text-sm text-gray-400">·</span>
+          </span>
+
+          {/* Author */}
+          <span className="inline-flex items-center space-x-2">
+            <Link to={storyPath} className="hover:underline">
+              by {author}
+            </Link>
+            <span className="text-sm text-gray-400">·</span>
+          </span>
+
+          {/* Time */}
+          <span className="inline-flex items-center space-x-2">
+            <Link to={storyPath} className="hover:underline">
+              {time}
+            </Link>
+            <span className="text-sm text-gray-400">·</span>
+          </span>
+
+          {/* Comments */}
+          <span className="inline-flex items-center">
+            <Link to={storyPath} className="hover:underline">
+              {comments} comments
+            </Link>
+          </span>
         </div>
       </div>
     </div>
